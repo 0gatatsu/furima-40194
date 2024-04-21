1d96945d5cefd2f116a535a73cc0aad1cbcd4ccc
0gatatsu <154909437+0gatatsu@users.noreply.github.com>
2024-04-21 16:29:30 +0900

    テストコード実装、Userモデル修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         validates :email, presence: true
-         validates :password, presence: true
-         validates :first_name_kanji, presence: true
-         validates :last_name_kanji, presence: true
-         validates :first_name_kana, presence: true
-         validates :last_name_kana, presence: true
-         validates :birthday, presence: true
-
    has_many :items
    has_many :comments
    has_many :orders
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX_ZENKAKU = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+  PASSWORD_REGEX_KATAKANA = /\A[ァ-ヶー－]+\z/.freeze
+
+  with_options presence: true do
+    validates :nickname
+    validates :email
+    validates :password, format: { with: PASSWORD_REGEX }
+    validates :first_name_kanji, format: { with: PASSWORD_REGEX_ZENKAKU }
+    validates :last_name_kanji, format: { with: PASSWORD_REGEX_ZENKAKU }
+    validates :first_name_kana, format: { with: PASSWORD_REGEX_KATAKANA }
+    validates :last_name_kana, format: { with: PASSWORD_REGEX_KATAKANA }
+    validates :birthday
+ end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    password              {'000000'}
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    first_name_kanji      {'漢字'}
+    last_name_kanji       {'漢字'}
+    first_name_kana       {'カタカナ'}
+    last_name_kana        {'カタカナ'}
+    birthday              {Faker::Date.birthday}
   end
 end
