7d39b3550d08f339b725138119f3735244fde55b
0gatatsu <154909437+0gatatsu@users.noreply.github.com>
2024-04-21 23:26:00 +0900

    Rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-   has_many :items
-   has_many :comments
-   has_many :orders
+  has_many :items
+  has_many :comments
+  has_many :orders
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  PASSWORD_REGEX_ZENKAKU = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
-  PASSWORD_REGEX_KATAKANA = /\A[ァ-ヶー－]+\z/.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  PASSWORD_REGEX_ZENKAKU = /\A[ぁ-んァ-ン一-龥]+\z/
+  PASSWORD_REGEX_KATAKANA = /\A[ァ-ヶー－]+\z/
 
   with_options presence: true do
     validates :nickname
@@ -21,5 +21,5 @@ class User < ApplicationRecord
     validates :first_name_kana, format: { with: PASSWORD_REGEX_KATAKANA }
     validates :last_name_kana, format: { with: PASSWORD_REGEX_KATAKANA }
     validates :birthday
- end
+  end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    first_name_kanji      {'漢字'}
-    last_name_kanji       {'漢字'}
-    first_name_kana       {'カタカナ'}
-    last_name_kana        {'カタカナ'}
-    birthday              {Faker::Date.birthday}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    first_name_kanji      { '漢字' }
+    last_name_kanji       { '漢字' }
+    first_name_kana       { 'カタカナ' }
+    last_name_kana        { 'カタカナ' }
+    birthday              { Faker::Date.birthday }
   end
 end
