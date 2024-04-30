b767eeb298cb26bd588101e7f2613c78eea42a66
0gatatsu <154909437+0gatatsu@users.noreply.github.com>
2024-05-01 02:33:22 +0900

    出品データ保存、バリデーション実装、そのた微修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,25 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :genre
+  belongs_to_active_hash :quality
+  belongs_to_active_hash :payment
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :day
+
+  validates :name, :image, :price, :content, :genre_id, :quality_id, :payment_id, :prefecture_id, :day_id, presence: true
+  validates :price, numericality: { less_than: 9_999_999, greater_than: 300 }
+
+  with_options numericality: { other_than: 0, message: "can't be blank"} do
+    validates :genre_id
+    validates :quality_id
+    validates :payment_id
+    validates :prefecture_id
+    validates :day_id
+  end
+
+  belongs_to :user
+  #has_many :comments
+  #has_one :order
+  has_one_attached :image
+
 end
