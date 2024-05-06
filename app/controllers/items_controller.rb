d0f25a7833b44ca8002c21e76cc32c7fbcc30950
0gatatsu <154909437+0gatatsu@users.noreply.github.com>
2024-05-06 14:59:15 +0900

    editアクション定義、(beforeアクション)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_action, only: [:edit, :show ]
 
   def index
     @items = Item.order('created_at DESC')
@@ -19,7 +20,10 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 
   private
@@ -28,4 +32,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :image, :price, :content, :genre_id, :quality_id, :payment_id, :prefecture_id, :days_id).merge(user_id: current_user.id)
   end
 
+  def set_action
+    @item = Item.find(params[:id])
+  end
 end
