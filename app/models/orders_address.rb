d13532d9531e13b3920093ecfe8b34825e195c2b
0gatatsu <154909437+0gatatsu@users.noreply.github.com>
2024-05-06 23:31:23 +0900

    addressモデル、マイグレーションファイル実装

--- app/models/orders_address.rb
+++ app/models/orders_address.rb
@@ -1,5 +1,5 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :post_number, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :token
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :token
 
 end
