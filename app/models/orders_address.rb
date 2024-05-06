1306c625dcbb2bb44eafe2fc325381847d07a72a
0gatatsu <154909437+0gatatsu@users.noreply.github.com>
2024-05-06 23:54:40 +0900

    データをテーブルに保存する処理

--- app/models/orders_address.rb
+++ app/models/orders_address.rb
@@ -13,5 +13,9 @@ class OrderAddress
     validates :token
   end
 
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create!(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
 
 end
