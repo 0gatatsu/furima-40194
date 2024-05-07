b89ad7d170f32117bc87054815aa7a6fe5d3b392
0gatatsu <154909437+0gatatsu@users.noreply.github.com>
2024-05-07 19:54:31 +0900

    フォームオブジェクトパターン実装、indexファイル編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,34 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_action, only: [:index, :create]
  
   def index
+    #@order_address = OrderAddress.new
   end
 
+  def new
+    @order_address = OrderAddress.new
+  end
 
   def create
+   
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id,:city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id,], token: params[:token])
+  end
+
+  def set_action
+    @item = Item.find(params[:item_id])
+  end
+
 end
