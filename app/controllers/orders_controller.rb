24bf932b4abde99d30ed9f58d9fa800ce982c5dc
0gatatsu <154909437+0gatatsu@users.noreply.github.com>
2024-05-08 16:53:09 +0900

    決済処理記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,11 @@ class OrdersController < ApplicationController
   before_action :set_action, only: [:index, :create]
  
   def index
-    #@order_address = OrderAddress.new
+    if current_user.id == @item.user_id
+      return redirect_to root_path
+    end
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+    @order_address = OrderAddress.new
   end
 
   def new
@@ -11,13 +15,14 @@ class OrdersController < ApplicationController
   end
 
   def create
-   
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-      @order_address.save
+      pay_item
+       @order_address.save
       redirect_to root_path
     else
-      render :new, status: :unprocessable_entity
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      render :index, status: :unprocessable_entity
     end
   end
 
@@ -25,6 +30,15 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id,:city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id,], token: params[:token])
+  end
+
+  def pay_item
+   Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+   Payjp::Charge.create(
+    amount: @item.price,  # 商品の値段
+    card: order_params[:token],    # カードトークン
+    currency: 'jpy'                 # 通貨の種類（日本円）
+   )
   end
 
   def set_action
